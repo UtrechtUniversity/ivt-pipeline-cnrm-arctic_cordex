a43c441fe2eff48304e1a2fca7ac733c76829b03
Linimie <lvgarderen86_6@hotmail.com>
2025-07-23 15:48:54 +0200

    Update run_ivt_pipeline.sh

--- run_ivt_pipeline.sh
+++ run_ivt_pipeline.sh
@@ -166,6 +166,6 @@ for YYYYMM in "${MONTH_LIST[@]}"; do
 done
 
 echo ">>> Cleaning up all files in ssp370_files directory ..."
-rm -v /ec/res4/scratch/nld1254/cnrm/6hourly/ssp370_files/*
+rm -v $MODEL_INPUT/ssp370_files/*
 
 echo "✅ All processing complete."
